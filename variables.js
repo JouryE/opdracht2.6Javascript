cd6fdbd4446b57e18975308a0d23eb88971b7c3b
JouryE <jourye@gmail.com>
2021-03-30 10:08:33 +0200

    variables gelukt!

--- variables.js
+++ variables.js
@@ -3,42 +3,57 @@
 // ==========================================
 
 const start = "Let's go!";
+console.log(start);
 
 // ==========================================
-// 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
+// 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven te worden.
 // ==========================================
+const firstName = "Joury";
+console.log(firstName);
 
 
 // ==========================================
-// 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
+// 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven te worden.
 // ==========================================
+const eyeColor = "Green";
+console.log(eyeColor);
 
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
+let age = 34;
+console.log(age);
 
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let profession = "Front End Developer!"
+console.log(profession);
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+const shoeSize = 45;
+console.log(shoeSize);
 
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
+let hasAPartner = true;
+console.log(hasAPartner);
 
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
+let numberOfChildren = 3;
+console.log(numberOfChildren);
 
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+console.log(start, firstName, eyeColor, age, profession, shoeSize, hasAPartner, numberOfChildren)
